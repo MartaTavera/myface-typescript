ce3e740913488a52d80db2ba78f55f8e0cc7b78d
MartaTavera <martatavera@gmail.com>
2024-02-01 15:40:06 +0000

    Top section of desing, plus fixed navbar. Co-authored-by: Nandini Polavarapu <nandini.p8892@gmail.com> Co-authored-by: Louisasa <Louisasa@users.noreply.github.com>

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Add your JavaScript here.
+const myElement = document.getElementById("change-color");
+
+myElement.addEventListener("click", function () {
+  const posts = document.getElementsByClassName("post_content");
+  console.log(posts);
+   [...posts].forEach(post=>post.style.background = 
+ ` rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`);
 });
+
+
+// function changeColor(){
+//   const myElement = document.getElementById('change-color');
+//   myElement.style.background = "rgb(100, 100, 100)";
+// }
+
+// const posts = document.getElementsByClassName("post_content");
+// [...posts].forEach(card=>{
+//   const changeButton=card.getElementById("change-color");
+//   changeButton.addEventListener("click",e=>{
+//     card.style.background = "rgb(100, 100, 100)";
+//   })
+// })
